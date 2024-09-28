00fb3c530245f4814c0d9ea692a5150f7dee7cf6
Biyu <120125620+CodingBiyu@users.noreply.github.com>
2024-09-28 22:10:59 +0500

    Update Tool.jsx

--- src/main-section/Tool.jsx
+++ src/main-section/Tool.jsx
@@ -1,11 +1,13 @@
 function Tool(props) {
   return (
-    <div className="tool-card">
-      <div className="bgImg">
+    <div className="tool-card" data-aos="slide-up" data-aos-duration="1000">
+      <div className="bgImg" data-aos="slide-up" data-aos-duration="1000">
         <img src={props.Image} />
       </div>
       <div className="toolInfo">
-        <h2>{props.Heading}</h2>
+        <h2 data-aos="slide-up" data-aos-duration="1000">
+          {props.Heading}
+        </h2>
         <p>{props.Description}</p>
       </div>
     </div>
